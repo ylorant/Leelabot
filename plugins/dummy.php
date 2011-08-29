10cc80516982ad31a06580fd0f8cce64c7e4b1aa
linkboss <linkboss@71f386ac-4425-4b13-9d66-67d17aa68f63>
2011-08-29 23:06:55 +0000

    Added documentation for dummy plugin

--- plugins/dummy.php
+++ plugins/dummy.php
@@ -1,5 +1,27 @@
 <?php
-
+/**
+ * \file plugins/dummy.php
+ * \author Yohann Lorant <linkboss@gmail.com>
+ * \version 0.5
+ * \brief Dummy plugin for Leelabot.
+ *
+ * \section LICENSE
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details at
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * \section DESCRIPTION
+ *
+ * This file contains the dummy plugin made for testing Leelabot core. As all test files, this will NOT be further documented.
+ */
 class PluginDummy
 {
 	private $_main;
