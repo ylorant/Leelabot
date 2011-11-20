515f0f0dfeb16069e3d9fe88eb8ba3625a21dcff
srwiez <srwiez@71f386ac-4425-4b13-9d66-67d17aa68f63>
2011-11-20 16:33:09 +0000

    debug irc.php

--- plugins/irc.php
+++ plugins/irc.php
@@ -49,8 +49,15 @@ class PluginIrc extends Plugin
 	public function init()
 	{
 		//Config
+		print_r($this->config);
 		if(isset($this->config['Server']) && isset($this->config['Port']) && isset($this->config['Nick']) && isset($this->config['User']) && isset($this->config['Channels']) && isset($this->config['MainChannel']) && isset($this->config['MessageMode']))
 		{
+			// Autospeak IRC <=> URT
+			if(isset($this->config['AutoSpeak']))
+				$this->config['AutoSpeak'] = $this->config['AutoSpeak'];
+			else
+				$this->config['AutoSpeak'] = 0;
+			
 			//Connection
 			$this->_connect();
 			
